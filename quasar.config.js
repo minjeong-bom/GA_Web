28322ef2238f0510713e067172cf9d1e36d8ac9d
minjeong <koominjeong99@gmail.com>
2023-09-26 10:57:15 +0900

    quasar.config.js에 iOS 빌드 설정

--- quasar.config.js
+++ quasar.config.js
@@ -170,7 +170,7 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-cordova-apps/configuring-cordova
     cordova: {
-      // noIosLegacyBuildFlag: true, // uncomment only if you know what you are doing
+      noIosLegacyBuildFlag: true, // uncomment only if you know what you are doing
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-capacitor-apps/configuring-capacitor

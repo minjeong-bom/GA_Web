bade81aae00f513728c7a8a538932e2aaac17bf5
minjeong <koominjeong99@gmail.com>
2023-11-29 13:43:59 +0900

    Axios 설치, BaseURL 설정

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -7,7 +7,10 @@ import axios from 'axios'
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'https://api.example.com' })
+const api = axios.create({
+  baseURL: 'https://combom.dev.go-sprint.co.kr/',
+  headers: 'SPRINT-API-KEY: SPRINTTEST',
+})
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
@@ -22,3 +25,40 @@ export default boot(({ app }) => {
 })
 
 export { api }
+
+
+// createArticle() {
+//   this.$api.post('/api/crud/create',
+//     {
+//       "data_prefix": "testing",
+//       "data_status": "on",
+//       "custom_data1": "hello",
+//       "custom_data2": "world"
+//     },
+//     {
+//       headers: {
+//         'SPRINT-API-KEY': 'sprinttest',
+//         'Content-Type': 'application/json', // JSON 형식의 본문을 보내는 경우에 필요,
+//       }
+//     }
+//   );
+// },
+// getItem() {
+//   this.$api.get(`/api/crud/single/OBLCCSLU`, {
+//     params: {
+//       'alias': 'mem',
+//     },
+//     headers: {
+//       'SPRINT-API-KEY': 'sprinttest',
+//     },
+//   })
+//     .then(response => {
+//       // 성공적인 응답 처리
+//       console.log(response.data);
+//     })
+//     .catch(error => {
+//       // 에러 처리
+//       console.error(error);
+//     });
+//
+// },

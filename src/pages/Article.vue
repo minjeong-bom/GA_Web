789a785e3325d7b414633e67cc65a73446016418
minjeongbb <minjung@companybb.com>
2024-01-03 18:30:09 +0900

    폰트 스타일 누락분 수정

--- src/pages/Article.vue
+++ src/pages/Article.vue
@@ -1,116 +1,32 @@
 <template>
-	<div class="article-layout">
-		<text-button-top-bar :title-text="this.categoryName"></text-button-top-bar>
-		<div class="user-profile-wrap">
-			<!-- creater info -->
-			<article-id style="width: 100%"
-			            :user-profile="''"
-			            :article-type="article.articleType"
-			            :view-count="article.viewCount"
-			            :creater-name="article.createrName"
-			            :job-title="article.createrJob"
-			            :created-at="article.createdAt"
-			            :control-ui="false"/>
-		</div>
-		<div class="headline-wrap">
-			<!-- headline -->
-			<h1 class="article-card-headline">{{ article.title }}</h1>
-			<!-- created at -->
-			<p class="article-created-at-text">{{ article.createdAt }}</p>
-		</div>
-
-		<div class="article-overview-wrap">
-			<!-- likes & comment -->
-			<div class="article-overview">
-				<span class="lable">공감</span>
-				<span class="value">10</span>
-				<span>∙</span>
-				<span class="lable">댓글</span>
-				<span class="value">{{ comments.length }}</span>
-			</div>
-			<div>
-				<!-- 상단 컨트롤 -->
-				<section>
-					<q-btn flat round icon="text_fields" style="opacity: 0.3"/>
-					<q-btn flat round icon="bookmark" @click="addBookmark()" style="opacity: 0.3"/>
-					<q-btn flat round icon="share" style="opacity: 0.3"/>
-				</section>
-			</div>
-		</div>
-		<!-- 본문 -->
-		<section v-if="isLoading" class="article-content">
-			<skeleton-line :lines="4"></skeleton-line>
-		</section>
-		<section v-else class="article-content" style="min-height: 200px" v-html="article.content"></section>
-		<!-- article controller -->
-		<section class="article-end-control-wrap">
-			<q-btn flat icon="text_fields" style="opacity: 0.3"/>
-			<q-btn flat icon="bookmark" style="opacity: 0.3"/>
-			<q-btn flat icon="share" style="opacity: 0.3"/>
-		</section>
-    <!-- end line -->
-		<div class="end-line">
-			<div></div>
-		</div>
-    <!-- control area -->
-		<div class="article-likes">
-			<div class="article-overview">
-				<span class="lable">공감</span>
-				<span class="value">10</span>
-				<span>∙</span>
-				<span class="lable">댓글</span>
-				<span class="value">{{ comments.length }}</span>
-			</div>
-			<div class="likes-list-area">
-				<!-- Add Like -->
-        <div class="add-likes-wrap">
-          <q-btn size="14px" flat round style="background: #FD384E" @click="showLikeButtons = !showLikeButtons">
-            <q-icon name="add" color="white"/>
-          </q-btn>
-	        <div v-show="showLikeButtons" class="likes-button-group">
-		        <img class="like-button" src="../assets/graphic/face-like.png" @click="createLike('like')"/>
-		        <img class="like-button" src="../assets/graphic/face-sad.png" @click="createLike('sad')"/>
-		        <img class="like-button" src="../assets/graphic/face-gido.png" @click="createLike('gido')"/>
-		        <img class="like-button" src="../assets/graphic/face-angry.png" @click="createLike('angry')"/>
-		        <img class="like-button" src="../assets/graphic/face-good.png" @click="createLike('good')"/>
-	        </div>
-        </div>
-				<!-- Like List -->
-				<section class="liker-slide">
-					<q-avatar v-for="item in likes" size="40px">
-						<div class="user-profile-wrap"></div>
-						<img class="imoji" :src="`src/assets/graphic/face-${item.empathy_log_title}.png`">
-					</q-avatar>
-				</section>
-				<!-- PageNation -->
-				<q-btn flat round @click="openLikeList()">
-					<q-icon name="navigate_next" style="opacity: 0.4;"/>
-				</q-btn>
-			</div>
-		</div>
-		<!-- create comments -->
-		<div class="comment-input-area">
-			<!-- profile image -->
-			<q-avatar size="40px">
-				<img src="https://cdn.quasar.dev/img/avatar.png">
-			</q-avatar>
-			<!-- comment input -->
-			<q-input dense rounded outlined v-model="addComment" :placeholder="commentInputPlaceholder" style="width: 100%;">
-				<template v-slot:append>
-					<q-btn flat round @click="createComment" style="right: -10px">
-						<img src="../assets/icon/ico_arrow-up-cycle.svg" style="width: 20px">
-					</q-btn>
-				</template>
-			</q-input>
-		</div>
-		<!-- comment list -->
-		<section class="l-column comment-list">
-			<div class="comment" v-for="item in comments">
-				<comment-id :writer="item.user_title" :user-position="item.user_data.job" :created-at="item.comment_regdate" />
-				<section class="comment-text">{{ item.comment_content }}</section>
-			</div>
-		</section>
-	</div>
+  <div class="article-layout">
+    <text-button-top-bar :title-text="this.categoryName"></text-button-top-bar>
+    <!-- 헤더 -->
+    <article-header :articleKey="articleKey" :article="article" :user-key="storageUserKey"/>
+    <!-- 댓글수 & 공감수, 북마크 & 공유 & 폰트크기 설정 -->
+    <div class="flex-sb article-overview-wrap">
+      <article-overview-info :likes-length="counts.likes" :comment-length="counts.comments" class="article-overview"/>
+      <article-controller :article-key="articleKey" :user-key="storageUserKey" @setFontSize="setFontSize"/>
+    </div>
+    <!-- 썸네일 -->
+    <skeleton-card v-if="isLoading" :lines="1"></skeleton-card>
+    <img v-else class="thumbnail-image-style" :src="'data:image/jpeg;base64,' + article.thumbnail">
+    <!-- 본문 -->
+    <section class="article-content">
+      <skeleton-line v-if="isLoading" :lines="4"></skeleton-line>
+      <p v-else v-html="article.content" :class="fontSizeClass"></p>
+    </section>
+    <!-- 구분선 -->
+    <div class="end-line"/>
+    <!-- 👍 댓글수 & 공감수, 좋아요 목록 -->
+    <div class="article-likes">
+      <article-overview-info :likes-length="counts.likes" :comment-length="counts.comments"/>
+      <like-stamp :article-key="articleKey" :user-key="storageUserKey" @likesCount="likesCount"/>
+    </div>
+    <!-- 댓글 입력, 댓글 목록 -->
+    <comment-u-i :storage-user-key="storageUserKey" :article-key="articleKey"
+                 @commentsCount="commentsCount"></comment-u-i>
+  </div>
 </template>
 
 <script>
